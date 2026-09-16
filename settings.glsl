659f3dee79f8b1190b69585ae92a0019ae71ed6b
Bloho <ayushsamanta904@gmail.com>
2026-09-16 21:16:15 +0530

    lighting values and contrast

--- settings.glsl
+++ settings.glsl
@@ -8,22 +8,22 @@
 #define BLOOM_THRESHOLD 0.85 // [0.6 0.7 0.75 0.8 0.85 0.9 1.0]
 #define BLOOM_RADIUS 3.0 // [1.0 2.0 3.0 4.0]
 
-#define BLOHO_LIGHTING 1 // []
-#define BLOHO_SHADOWS 1 // []
-#define LIGHTING_STRENGTH 1.0 // []
-#define TORCH_WARMTH 0.7 // []
-#define EMISSIVE_STRENGTH 1.5 // []
-#define BLOHO_ORES 1 // []
-#define ORE_GLOW_STRENGTH 2.0 // []
-#define NEUTRAL_ORE_GLOW
-#define BLOHO_WATER 1 // []
+#define BLOHO_LIGHTING 1 // [0 1]
+#define BLOHO_SHADOWS 1 // [0 1]
+#define LIGHTING_STRENGTH 1.0 // [0.0 0.5 0.75 1.0]
+#define TORCH_WARMTH 0.7 // [0.0 0.35 0.7 1.0]
+#define EMISSIVE_STRENGTH 1.5 // [0.0 0.5 1.0 1.5 2.0]
+#define BLOHO_ORES 1 // [0 1]
+#define ORE_GLOW_STRENGTH 2.0 // [0.0 0.5 1.0 1.5 2.0 3.0 4.0]
+#define NEUTRAL_ORE_GLOW 0 // [0 1]
+#define BLOHO_WATER 1 // [0 1]
 
-#define BLOHO_GRADE 1 // []
-#define EXPOSURE
-#define CONTRAST
-#define SATURATION
-#define COLOR_WARMTH
-#define VIGNETTE_STRENGTH
+#define BLOHO_GRADE 1 // [0 1]
+#define EXPOSURE 1.08 // [0.8 0.9 1.0 1.08 1.2 1.35]
+#define CONTRAST 1.18 // [1.0 1.08 1.18 1.28 1.4]
+#define SATURATION 1.08 // [0.85 1.0 1.08 1.16 1.25]
+#define COLOR_WARMTH 0.25 // [-0.5 -0.25 0.0 0.25 0.5]
+#define VIGNETTE_STRENGTH 0.08 // [0.0 0.04 0.08 0.12 0.2]
 
 #define BLOHO_FOG
 #define FOG_STRENGTH
