19449711386c4a9ff1927aa84d8d12ddd3fe463e
Bloho <ayushsamanta904@gmail.com>
2026-09-16 21:09:58 +0530

    all the params in settings page

--- settings.glsl
+++ settings.glsl
@@ -1,2 +1,28 @@
 #ifndef BLOHO_SETTINGS
 #define BLOHO_SETTINGS
+
+#define BLOHO_TITLE 0 // [0]
+
+#define BLOHO_BLOOM 1 // [0 1]
+#define BLOOM_STRENGTH 0.3 // []
+#define BLOOM_THRESHOLD 0.85 // []
+#define BLOOM_RADIUS 3.0 // []
+
+#define BLOHO_LIGHTING 1 // []
+#define BLOHO_SHADOWS 1 // []
+#define LIGHTING_STRENGTH 1.0 // []
+#define TORCH_WARMTH 0.7 // []
+#define EMISSIVE_STRENGTH 1.5 // []
+#define BLOHO_ORES 1 // []
+#define ORE_GLOW_STRENGTH 2.0 // []
+#define NEUTRAL_ORE_GLOW
+#define BLOHO_WATER 1 // []
+
+
+
+
+
+
+
+
+#endif
