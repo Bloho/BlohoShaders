45d49343accb7f792780d51d7be8167378667f42
Bloho <ayushsamanta904@gmail.com>
2026-09-16 05:44:51 +0530

    adding overlays to the fog

--- lib/fog.glsl
+++ lib/fog.glsl
@@ -17,5 +17,24 @@ float blohoVisibility(vec3 viewPosition) {
         visibility = 1.0 - smoothstep(fogStart, fogEnd, distanceToEye);
     } else if (fogMode == 2048) {
         visibility = exp(-max(fogDensity, 0.0) * distanceToEye)
-    } else if ()
+    } else if (fogMode == 2049) {
+        float opticalDistance = max(fogDensity, 0.0) * distanceToEye;
+        visibility = exp(-opticalDistance * opticalDistance);
+    }
+
+    if (far > 0.0) {
+        float hazaDistance = max(distanceToEye - 16.0, 0.0);
+        float density = FOG_STRENGTH * (1.0 + 1.8 * rainStrength) / max(far * 2.8, 128.0);
+        visibility *= exp(-hazeDistance * density);
+    }
+    return clamp(visibility, 0.0, 1.0);
+}
+
+vec3 blohoFog(vec3 color, vec3 viewPosition) {
+    float visibility = blohoVisibility(viewPosition);
+#ifdef BLOHO_ADDITIVE
+    return color * visibility;
+#else
+    return mix(fogColor, color, visibility);
+#endif
 }
